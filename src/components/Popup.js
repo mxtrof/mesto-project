f8109618844ba28b44e592660e682d33407418d4
alugibaN <budda.bob@mail.ru>
2023-07-14 22:09:45 +0500

    proverka

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,37 +1,11 @@
-// import {getFormElements} from './validate.js';
-
-// const closeByEsc = (evt) => {
-//     if (evt.key === "Escape") {
-//         const openedPopup = document.querySelector('.popup_opened');
-//         closeModal(openedPopup);
-//     }
-// }
-
-// export const openModal = (modal) => {
-//     modal.classList.add('popup_opened');
-//     document.addEventListener('keydown', closeByEsc);
-// }
-
-// export const closeModal = (elem) => {
-//     elem.classList.remove('popup_opened');
-//     document.removeEventListener('keydown', closeByEsc);
-// }
-
-// export const closeModalClickHandler = (evt) => {
-//     // проверим, что элемент, на который кликнули содердит класс модал-клоус 
-//     if ((evt.target.classList.contains('modal-close')) || (evt.target === evt.currentTarget)) {
-//         closeModal(evt.currentTarget);
-//     }
-// }
-
 
 export class Popup { 
     constructor(popupSelector){
       this._popup = document.querySelector(popupSelector)
     }
   
-   openModal() {
-      this._popup.classList.add('popup_opened');
+   openModal () {
+      this._popup .classList.add('popup_opened');
       document.addEventListener('keydown', this._closeByEsc)
       this.setEventListeners()
   }
